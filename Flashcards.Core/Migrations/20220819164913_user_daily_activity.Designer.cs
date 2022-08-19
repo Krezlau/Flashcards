4dc8f0d6169fedca09d530cc35ec0bba9021a6b6
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-08-19 18:50:43 +0200

    daily activity model and migration

--- Flashcards.Core/Migrations/20220819164913_user_daily_activity.Designer.cs
+++ Flashcards.Core/Migrations/20220819164913_user_daily_activity.Designer.cs
@@ -4,14 +4,16 @@ using Flashcards.Core.DBConnection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Flashcards.Core.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20220819164913_user_daily_activity")]
+    partial class user_daily_activity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Flashcards.Core/Migrations/20220819164913_user_daily_activity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Flashcards.Core.Migrations
+{
+    public partial class user_daily_activity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyActivity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Day = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReviewedFlashcardsCount = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyActivity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DailyActivity_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DailyActivity_UserId",
+                table: "DailyActivity",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyActivity");
+        }
+    }
+}
