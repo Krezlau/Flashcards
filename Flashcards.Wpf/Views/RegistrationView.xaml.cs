0ca50ee4b4e3249ed0aedffce110352b15009f37
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-08-24 18:53:23 +0200

    LogIn and Registration Views adjustments

--- Flashcards.Wpf/Views/RegistrationView.xaml.cs
+++ Flashcards.Wpf/Views/RegistrationView.xaml.cs
@@ -29,13 +29,13 @@ namespace Flashcards.WpfApp.Views
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
             if (this.DataContext != null)
-            { ((dynamic)this.DataContext).Password = ((PasswordBox)sender).Password; }
+            { ((dynamic)this.DataContext).Password = ((Wpf.Ui.Controls.PasswordBox)sender).Password; }
         }
 
         private void PasswordBox_ConfirmPasswordChanged(object sender, RoutedEventArgs e)
         {
             if (this.DataContext != null)
-            { ((dynamic)this.DataContext).ConfirmPassword = ((PasswordBox)sender).Password; }
+            { ((dynamic)this.DataContext).ConfirmPassword = ((Wpf.Ui.Controls.PasswordBox)sender).Password; }
         }
     }
 }
