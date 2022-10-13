bbedd7d665a1f7a61139128d6711b5120fc096e8
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-10-13 10:57:54 +0200

    migration

--- Flashcards.Core/Migrations/20221013085703_ChangeMinutesSpentType.Designer.cs
+++ Flashcards.Core/Migrations/20221013085703_ChangeMinutesSpentType.Designer.cs
@@ -4,14 +4,16 @@ using Flashcards.Core.DBConnection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Flashcards.Core.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20221013085703_ChangeMinutesSpentType")]
+    partial class ChangeMinutesSpentType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Flashcards.Core/Migrations/20221013085703_ChangeMinutesSpentType.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Flashcards.Core.Migrations
+{
+    public partial class ChangeMinutesSpentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "MinutesSpentLearning",
+                table: "DeckActivity",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "MinutesSpentLearning",
+                table: "DailyActivity",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinutesSpentLearning",
+                table: "DeckActivity",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MinutesSpentLearning",
+                table: "DailyActivity",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
