a94001d9d04027a8f5af9ecc5fdb919e1065c65a
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-11-14 13:21:58 +0100

    LoadingService

---
+++ Flashcards.Wpf/Services/LoadingRingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Wpf.Ui.Controls;
+
+namespace Flashcards.WpfApp.Services
+{
+    public class LoadingRingService
+    {
+        private Border _loadingScreen;
+
+        public void SetLoadingScreen(Border loadingScreen)
+        {
+            _loadingScreen = loadingScreen;
+        }
+
+        public Border GetLoadingScreen()
+        {
+            if (_loadingScreen is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _loadingScreen;
+        }
+    }
+}
---
+++ Flashcards.Wpf/Services/WpfLoadingService.cs
@@ -0,0 +1,29 @@
+﻿using Flashcards.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.WpfApp.Services
+{
+    public class WpfLoadingService : ILoadingService
+    {
+        private readonly LoadingRingService _loadingRingService;
+
+        public WpfLoadingService(LoadingRingService loadingRingService)
+        {
+            _loadingRingService = loadingRingService;
+        }
+
+        public void Enable()
+        {
+            _loadingRingService.GetLoadingScreen().Visibility = System.Windows.Visibility.Visible;
+        }
+
+        public void Disable()
+        {
+            _loadingRingService.GetLoadingScreen().Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
