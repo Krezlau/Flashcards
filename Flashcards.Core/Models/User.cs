e8ac388ba0482671c51330cdd66c3906baebf663
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2022-11-16 18:11:44 +0100

    userTests, FutureReviewsOrganizerTests bug fix

--- Flashcards.Core/Models/User.cs
+++ Flashcards.Core/Models/User.cs
@@ -67,6 +67,7 @@ namespace Flashcards.Core.Models
 
             int i = daysRegisteredList.Count - 1;
             int sum = 0;
+            daysRegisteredList.OrderBy(d => d);
             while (i >= 0 && date == daysRegisteredList[i--].Date)
             {
                 date = date.AddDays(-1);
